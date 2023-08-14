0b03cd5f6c2b194cb272d0dea45f7367626c453f
TizianoBatistella <tizibatis2@gmail.com>
2023-08-14 17:48:28 -0300

    aaaa

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,11 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-
-    <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway|Ubuntu" rel="stylesheet">
-
-    <!-- Estilos -->
     <link rel="stylesheet" href="css/login.css">
 
     <title>Formulario Login y Registro de Usuarios</title>
